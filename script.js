9dd9a7e905c18b9b742324936b62b6dbbb1a9876
laptopmahsasalehi-boop <laptopmahsasalehi@gmail.com>
2026-08-29 23:01:00 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.getElementById("menuButton");
     const mobileMenu = document.getElementById("mobileMenu");
 
-    if (menuButton && mobileMenu) {
+    if (!menuButton || !mobileMenu) return;
 
-        menuButton.addEventListener("click", function () {
-            mobileMenu.classList.toggle("active");
+    // باز و بسته شدن منو
+    menuButton.addEventListener("click", function () {
+        mobileMenu.classList.toggle("active");
+    });
+
+
+    // بستن منو بعد از انتخاب هر گزینه
+    const menuLinks = mobileMenu.querySelectorAll("a");
+
+    menuLinks.forEach(function (link) {
+
+        link.addEventListener("click", function () {
+
+            mobileMenu.classList.remove("active");
+
         });
 
-    }
+    });
 
 });
-
